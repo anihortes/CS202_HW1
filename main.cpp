c2b6ef91b237507d360f2630ba7d99e745b868e0
Adam Owen <ahowenjr@alaska.edu>
2021-01-29 21:32:15 -0900

    properly reads file and copies into a vector

--- main.cpp
+++ main.cpp
@@ -1,32 +1,44 @@
-#include "StopWatch.h"
+//#include "StopWatch.h"
 #include <iostream>
-#include <vector>
-#include <fstream>
-#include <string>
 using std::cout;
 using std::endl;
+#include <vector>
 using std::vector;
-using std::string;
+#include <fstream>
 using std::ifstream;
+#include <string>
+using std::string;
 
 
 int main() {
-
+    // must declare with {""} or else push_back ignore first instance
+    vector <string> fileVector = {""};
+    string nameCopy;
     ifstream myFile;
     string fileName = "big.txt";
-    myFile.open(fileName);
+    myFile.open(fileName, std::ios::in | std::ios::binary);
     if(!myFile){
         cout << "file not found." << endl;
+        return 0;
     }
+    else {
+        cout << "file found." << endl;
+        //print file into vector
+        while(myFile >> nameCopy){
+            cout << "test: " << nameCopy << endl;
+            fileVector.push_back(nameCopy);
+        }
+        //ALWAYS CLOSE FILE WHEN DONE READING
+        myFile.close();
 
-
-    //int x;
-    //StopWatch timer;
-    //timer.start();
-    //timer.stop();
-    //cout << "Start Time: " << timer.start() << endl;
-    //cout << "Stop Time: " << timer.stop() << endl;
-    //auto endTime = timer.stop()-timer.start();
-    //std::cout << "time passed: " << endTime << std::endl;
+        //int x;
+        //StopWatch timer;
+        //timer.start();
+        //timer.stop();
+        //cout << "Start Time: " << timer.start() << endl;
+        //cout << "Stop Time: " << timer.stop() << endl;
+        //auto endTime = timer.stop()-timer.start();
+        //std::cout << "time passed: " << endTime << std::endl;
+    }
     return 0;
 }
