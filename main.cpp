319bc1238391b21ea76e58e04c14be6b7da0c4d9
Adam Owen <ahowenjr@alaska.edu>
2021-01-30 15:40:58 -0900

    added file reading with list function.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using std::vector;
 using std::ifstream;
 #include <string>
 using std::string;
+#include <list>
 
 vector<string> fileReadWithVector(const string &fileName){
     // must declare with {""} or else push_back ignore first instance
@@ -28,6 +29,26 @@ vector<string> fileReadWithVector(const string &fileName){
         myFile.close();
     }
     return fileVector;
+}
+
+std::list<string> fileReadWithList(const string &fileName){
+    std::list<string> fileList;
+    string nameCopy;
+    ifstream myFile;
+    myFile.open(fileName, std::ios::in | std::ios::binary);
+    if(!myFile){
+        cout << "file not found." << endl;
+    }
+    else {
+        cout << "file found." << endl;
+        //print file into vector
+        while (myFile >> nameCopy) {
+            fileList.push_back(nameCopy);
+        }
+        //ALWAYS CLOSE FILE WHEN DONE READING
+        myFile.close();
+    }
+    return fileList;
 }
 
 int main() {
@@ -56,12 +77,18 @@ int main() {
             getline(std::cin, bookTitle);
         }
     }
-    StopWatch timer;
-    timer.start();
+    StopWatch vectorTimer;
+    vectorTimer.start();
     fileReadWithVector(fileName);
-    timer.stop();
-    std::cout << "Time to read with vector (seconds): " << timer.timeSecond() << std:: endl;
-    std::cout << "Time to read vector (milliseconds): " << timer.timeMilliSec()<< std::endl;
+    vectorTimer.stop();
+    std::cout << "Time to read with vector (seconds): " << vectorTimer.timeSecond() << std:: endl;
+    std::cout << "Time to read vector (milliseconds): " << vectorTimer.timeMilliSec()<< std::endl;
+    StopWatch listTimer;
+    listTimer.start();
+    fileReadWithList(fileName);
+    listTimer.stop();
+    std::cout << "\nTime to read with list (seconds): " << listTimer.timeSecond() << std:: endl;
+    std::cout << "Time to read list (milliseconds): " << listTimer.timeMilliSec()<< std::endl;
 
     return 0;
 }
