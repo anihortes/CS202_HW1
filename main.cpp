ccc588fa7328d62ea18578a5f35c62a75b405d82
Adam Owen <ahowenjr@alaska.edu>
2021-01-30 15:20:55 -0900

    program asks user to select file to read adding timing benchmarks for reading vector

--- main.cpp
+++ main.cpp
@@ -9,40 +9,59 @@ using std::ifstream;
 #include <string>
 using std::string;
 
-
-int main() {
-    StopWatch timer;
-    timer.start();
-    auto t1 = std::chrono::high_resolution_clock::now();
+vector<string> fileReadWithVector(const string &fileName){
     // must declare with {""} or else push_back ignore first instance
     vector <string> fileVector = {""};
     string nameCopy;
     ifstream myFile;
-    string fileName = "big.txt";
     myFile.open(fileName, std::ios::in | std::ios::binary);
     if(!myFile){
         cout << "file not found." << endl;
-        return 0;
     }
     else {
         cout << "file found." << endl;
         //print file into vector
-        while(myFile >> nameCopy){
-            cout << "test: " << nameCopy << endl;
+        while (myFile >> nameCopy) {
             fileVector.push_back(nameCopy);
         }
         //ALWAYS CLOSE FILE WHEN DONE READING
         myFile.close();
-
-        int x;
-        std::cin >> x;
-        timer.stop();
-        auto t2 = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> fp_ms = t2-t1;
-        auto endTime = timer.milliSec();
-      //  timer.milliSec(timer.start(), timer.stop());
-      std::cout << "test time: " << fp_ms.count() << std:: endl;
-        std::cout << "time passed: " << endTime << std::endl;
     }
+    return fileVector;
+}
+
+int main() {
+    string fileName;
+    cout << "Enter a number to select a file that you want to read." << endl;
+    cout << "1) Frankenstein by Mary Shelley" << endl;
+    cout << "2) Jane Eyre by Charlotte Bronte" << endl;
+    cout << "Enter here:";
+    string bookTitle;
+    getline(std::cin, bookTitle);
+    while(true){
+        if(bookTitle == "1"){
+            fileName = "frankenstein.txt";
+            break;
+        }
+        else if(bookTitle == "2"){
+            fileName = "jane eyre.txt";
+            break;
+        }
+        else {
+            cout << "Program cannot understand." << endl;
+            cout << "Please enter only the number to select a file to read.\n" << endl;
+            cout << "1) Frankenstein by Mary Shelley" << endl;
+            cout << "2) Jane Eyre by Charlotte Bronte" << endl;
+            cout << "Enter here: ";
+            getline(std::cin, bookTitle);
+        }
+    }
+    StopWatch timer;
+    timer.start();
+    fileReadWithVector(fileName);
+    timer.stop();
+    std::cout << "Time to read with vector (seconds): " << timer.timeSecond() << std:: endl;
+    std::cout << "Time to read vector (milliseconds): " << timer.timeMilliSec()<< std::endl;
+
     return 0;
 }
